d7239a21ecc029b674be5f30a1aedc82e43a133b
ahmedouda1995 <ahmedouda.1995@hotmail.com>
2018-02-22 20:17:08 +0200

    Added jwt check in signout

--- src/main/java/userCommands/SignOutCommand.java
+++ src/main/java/userCommands/SignOutCommand.java
@@ -1,5 +1,11 @@
 package userCommands;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+
+import java.io.UnsupportedEncodingException;
+import java.security.SignatureException;
 import java.util.HashMap;
 import java.security.MessageDigest;
 import java.util.LinkedHashMap;
@@ -7,11 +13,38 @@ import javax.xml.bind.DatatypeConverter;
 
 public class SignOutCommand extends abstraction.Command {
 
+    private static final  String secretKey = "NpapHelGNRvAWEc0XGLYDJI83rdo5yJp1sxAS";
+
     public SignOutCommand(HashMap<String, String> args) {
         super(args);
     }
 
-    public LinkedHashMap<String, Object> execute() {
-        return null; // wb7CMF
+    public LinkedHashMap<String, Object> execute() throws UnsupportedEncodingException {
+
+        LinkedHashMap<String, Object> response = new LinkedHashMap<String, Object>();
+        String jwtToken = args.get("jwtToken");
+
+        if(jwtToken != null) {
+            try {
+                //OK, we can trust this JWT
+                Jws<Claims> claims = Jwts.parser()
+                        .setSigningKey(secretKey.getBytes("UTF-8"))
+                        .parseClaimsJws(jwtToken);
+                System.out.println("Succcess");
+            } catch (io.jsonwebtoken.SignatureException e) {
+                System.out.println(e.getMessage());
+                //don't trust the JWT!
+            } catch (io.jsonwebtoken.ExpiredJwtException e){
+                System.out.println(e.getMessage());
+            }
+            catch (Exception e) {
+
+            }
+        }
+        else {
+
+        }
+
+        return response;
     }
 }
