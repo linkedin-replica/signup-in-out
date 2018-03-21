fcca45c27bc4e515207c1648e23af9f1245e9eea
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2018-03-21 17:55:17 +0200

    Arangodb tests :sparkles:

--- src/main/java/com/linkedin/replica/signing/tests/ArangoSigningHandlerTest.java
+++ src/main/java/com/linkedin/replica/signing/tests/ArangoSigningHandlerTest.java
@@ -1,82 +1,122 @@
 package com.linkedin.replica.signing.tests;
 
+import com.arangodb.ArangoCursor;
+import com.arangodb.ArangoDatabase;
+import com.arangodb.entity.DocumentCreateEntity;
+import com.linkedin.replica.signing.database.handlers.impl.ArangoSigningHandler;
+import com.linkedin.replica.signing.database.DatabaseConnection;
+import com.linkedin.replica.signing.models.User;
+import org.junit.*;
+import com.linkedin.replica.signing.config.Configuration;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class ArangoSigningHandlerTest {
+
+    private static ArangoSigningHandler arangoSigningHandler;
+    private static ArangoDatabase arangoDb;
+    static Configuration config;
+
+    @BeforeClass
+    public static void init() throws IOException, SQLException {
+        String rootFolder = "src/main/resources/config/";
+        Configuration.init(rootFolder + "app.config",
+                rootFolder + "arango.config",
+                rootFolder+ "mysql.config",
+                rootFolder + "command.config");
+
+        DatabaseConnection.init();
+        config = Configuration.getInstance();
+        arangoSigningHandler = new ArangoSigningHandler();
+        arangoDb = DatabaseConnection.getInstance().getArangoDriver().db(
+                Configuration.getInstance().getArangoConfig("db.name")
+        );
+    }
+
+    @Before
+    public void initBeforeTest() {
+        arangoDb.createCollection(
+                config.getArangoConfig("collection.name")
+        );
+    }
+
+    static DocumentCreateEntity createUser(User user){
+        return arangoDb.collection(config.getArangoConfig("collection.name")).insertDocument(user);
+    }
+    static void deleteUser(String userId){
+        arangoDb.collection(config.getArangoConfig("collection.name")).deleteDocument(userId);
+    }
+
+    static User getUser(String userId) {
+        return arangoDb.collection(config.getArangoConfig("collection.name")).getDocument(userId, User.class);
+    }
+
+    @Test
+    public void testCreateUser() {
+
+        String email = "example.mail@gmail.com";
+        String firstname = "User firstname";
+        String lastname = "User lastname";
+
+        User userProfile = new User();
+
+        userProfile.setEmail(email);
+        userProfile.setFirstName(firstname);
+        userProfile.setLastName(lastname);
+
+
+        String userId = arangoSigningHandler.createUser(userProfile);
+        assertEquals(String.format("The user of email: %s should exist in users collection", email), arangoDb.collection(config.getArangoConfig("collection.name")).documentExists(userId), true);
+
+        User insertedUser = getUser(userId);
+        assertEquals(String.format("Expected both users have the same email: %s", email), email, insertedUser.getEmail());
+        assertEquals(String.format("Expected both users have the same firstname: %s", firstname), firstname, insertedUser.getFirstName());
+        assertEquals(String.format("Expected both users have the same lastname: %s", lastname), lastname, insertedUser.getLastName());
+
+        deleteUser(userId);
+        assertEquals("The user should be deleted",arangoDb.collection(config.getArangoConfig("collection.name")).documentExists(userId), false);
+    }
+
+    @Test
+    public void testGetUser() {
+        String email = "example.mail@gmail.com";
+        String firstname = "User firstname";
+        String lastname = "User lastname";
+
+        User userProfile = new User();
+
+
+        userProfile.setEmail(email);
+        userProfile.setFirstName(firstname);
+        userProfile.setLastName(lastname);
+
+        DocumentCreateEntity user = createUser(userProfile);
+
+        User newUser = (User) arangoSigningHandler.getUser(user.getKey());
+        assertEquals(String.format("Expected both users have the same email: %s", email), email, newUser.getEmail());
+        assertEquals(String.format("Expected both users have the same firstname: %s", firstname), firstname, newUser.getFirstName());
+        assertEquals(String.format("Expected both users have the same lastname: %s", lastname), lastname, newUser.getLastName());
+
+        deleteUser(newUser.getId());
+        assertEquals("The user should be deleted",arangoDb.collection(config.getArangoConfig("collection.name")).documentExists(user.getKey()), false);
+    }
+
 //
-//    private static ArangoSigningHandler arangoDatabaseHandler;
-//    private static ArangoDatabase arangoDb;
-//    static Properties config;
-//
-//    @BeforeClass
-//    public static void init() {
-//        config = readDatabaseConfig();
-//        arangoDatabaseHandler = new ArangoSigningHandler();
-//        arangoDatabaseHandler.connect();
-//        arangoDb = arangoDatabaseHandler.getDBConnection().getArangoDriver().db(
-//                config.getProperty("db.name")
-//        );
-//    }
-//
-//    @Before
-//    public void initBeforeTest() {
-//        arangoDb.collection(
-//                config.getProperty("collection.users.name")
-//        ).drop();
-//
-//        arangoDb.createCollection(
-//                config.getProperty("collection.users.name")
-//        );
-//    }
-//
-//    @Test
-//    public void testCreateUser() {
-//
-//        String email = "nabila.ahmed@gmail.com";
-//
-//        UserProfile userProfile = UserProfile.Instantiate();
-//
-//        userProfile.setEmail(email);
-//        userProfile.setFirstName("Nabila");
-//        userProfile.setLastName("Ahmed");
-//
-//
-//        String userId = arangoDatabaseHandler.createUser(userProfile);
-//        assertEquals(String.format("The user of email: %s should exist in users collection", email), arangoDb.collection(config.getProperty("collection.users.name")).documentExists(userId), true);
-//
-//        arangoDb.collection(config.getProperty("collection.users.name")).deleteDocument(userId);
-//        assertEquals("The user should be deleted",arangoDb.collection(config.getProperty("collection.users.name")).documentExists(userId), false);
-//    }
-//
-//    @Test
-//    public void testGetUser() {
-//        String email = "nabila.ahmed@gmail.com";
-//        String firstname = "Nabila";
-//        String lastname = "Ahmed";
-//        String key = "1";
-//
-//        UserProfile userProfile = UserProfile.Instantiate();
-//
-//        userProfile.setKey(key);
-//        userProfile.setEmail(email);
-//        userProfile.setFirstName(firstname);
-//        userProfile.setLastName(lastname);
-//
-//        DocumentCreateEntity user = arangoDb.collection(config.getProperty("collection.users.name")).insertDocument(userProfile);
-//
-//        UserProfile newUser = (UserProfile) arangoDatabaseHandler.getUser(key);
-//        assertEquals(String.format("Expected both users have the same email: %s", email), email, newUser.getEmail());
-//        assertEquals(String.format("Expected both users have the same firstname: %s", firstname), firstname, newUser.getFirstName());
-//        assertEquals(String.format("Expected both users have the same lastname: %s", lastname), lastname, newUser.getLastName());
-//
-//        arangoDb.collection(config.getProperty("collection.users.name")).deleteDocument(user.getKey());
-//        assertEquals("The user should be deleted",arangoDb.collection(config.getProperty("collection.users.name")).documentExists(user.getKey()), false);
-//    }
-//
-//
-//    @AfterClass
-//    public static void clean(){
-//
-//        arangoDatabaseHandler.getDBConnection().disconnect();
-//    }
+    @After
+    public void cleanAfterTest() {
+        arangoDb.collection(
+                config.getArangoConfig("collection.name")
+        ).drop();
+    }
+
+    @AfterClass
+    public static void clean() throws SQLException {
+        DatabaseConnection.getInstance().closeConnections();
+    }
 }
